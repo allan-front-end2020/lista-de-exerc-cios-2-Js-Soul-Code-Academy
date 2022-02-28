38ab4c4b95dafec2d27cbab3fa978960cee22da5
Allan Tavares <allan.tavares@estudante.ufla.br>
2022-02-27 21:16:07 -0300

    exercicio 2 realizado com sucesso

---
+++ atividade-1/style.js
@@ -0,0 +1,11 @@
+const menssagem = prompt('digite sua menssagem')
+
+
+document.write(menssagem.toUpperCase())
+
+
+document.write(menssagem.toLowerCase())
+
+var letra = menssagem.split('')
+
+console.log(letra)
